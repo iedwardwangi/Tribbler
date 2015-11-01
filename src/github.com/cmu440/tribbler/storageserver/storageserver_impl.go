f810a21f17d7299e2bbed06841abf5085d9b6f2c
Edward Wang <edwardwang@CMU-880864.WV.CC.CMU.EDU>
2015-10-31 22:30:19 -0400

    Struct in storage server

--- src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
+++ src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
@@ -2,12 +2,24 @@ package storageserver
 
 import (
 	"errors"
-
 	"github.com/cmu440/tribbler/rpc/storagerpc"
+	"sync"
 )
 
+// Value struct for single value(string type)
+type sValue struct {
+	value string
+}
+
+type lValue struct {
+	value []string
+}
+
 type storageServer struct {
-	// TODO: implement this!
+	sMutex sync.Mutex // Mutex for single value map
+	lMutex sync.Mutex // Mutex for list value map
+	sMap   map[string]*sValue
+	lMap   map[string]*lValue
 }
 
 // NewStorageServer creates and starts a new StorageServer. masterServerHostPort
@@ -31,11 +43,48 @@ func (ss *storageServer) GetServers(args *storagerpc.GetServersArgs, reply *stor
 }
 
 func (ss *storageServer) Get(args *storagerpc.GetArgs, reply *storagerpc.GetReply) error {
-	return errors.New("not implemented")
+	if args == nil {
+		return errors.New("ss: Can't get nil K/V pair")
+	}
+	if reply == nil {
+		return errors.New("ss: Can't reply with nil in Get")
+	}
+	ss.sMutex.Lock()
+	defer ss.sMutex.Unlock()
+
+	// TODO: check key in range for the server
+
+	val, ok := ss.sMap[args.Key]
+	if ok {
+		reply.Value = val.value
+		reply.Status = OK
+	} else {
+		reply.Status = KeyNotFound
+	}
+	return nil
 }
 
 func (ss *storageServer) Delete(args *storagerpc.DeleteArgs, reply *storagerpc.DeleteReply) error {
-	return errors.New("not implemented")
+	if args == nil {
+		return errors.New("ss: Can't delete nil K/V pair")
+	}
+	if reply == nil {
+		return errors.New("ss: Can't reply with nil in Delete")
+	}
+	ss.sMutex.Lock()
+	defer ss.sMutex.Unlock()
+
+	// TODO: check key in range for the server
+
+	val, ok := ss.sMap[args.Key]
+	if ok {
+		delete(ss.sMap[args.Key])
+		reply.Status = OK
+	} else {
+		reply.Status = KeyNotFound
+	}
+
+	return nil
 }
 
 func (ss *storageServer) GetList(args *storagerpc.GetArgs, reply *storagerpc.GetListReply) error {
@@ -43,11 +92,53 @@ func (ss *storageServer) GetList(args *storagerpc.GetArgs, reply *storagerpc.Get
 }
 
 func (ss *storageServer) Put(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
-	return errors.New("not implemented")
+	if args == nil {
+		return errors.New("ss: Can't put nil K/V pair")
+	}
+	if reply == nil {
+		return errors.New("ss: Can't reply with nil in Put")
+	}
+	ss.sMutex.Lock()
+	defer ss.sMutex.Unlock()
+
+	// TODO: check key in range for the server
+
+	newValue := sValue{
+		value: args.Value,
+	}
+	ss.sMap[args.Key] = &newValue
+	reply.Status = OK
+	return nil
 }
 
 func (ss *storageServer) AppendToList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
-	return errors.New("not implemented")
+	if args == nil {
+		return errors.New("ss: Can't append nil K/V pair")
+	}
+	if reply == nil {
+		return errors.New("ss: Can't reply with nil in Append")
+	}
+	ss.lMutex.Lock()
+	defer ss.lMutex.Unlock()
+
+	// TODO: check key in range for the server
+
+	lst, ok := ss.lMap[args.Key]
+	if ok {
+
+	} else {
+		newValue := lValue{
+			value := make([]string, 1)
+		}
+		
+		ss.lMap[args.Key] = 
+	}
+	newValue := sValue{
+		value: args.Value,
+	}
+	ss.sMap[args.Key] = &newValue
+	reply.Status = OK
+	return nil
 }
 
 func (ss *storageServer) RemoveFromList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
